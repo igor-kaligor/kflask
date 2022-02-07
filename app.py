9dfe9b809f7e164d52261ef7a0aa190cf8149157
igor-kaligor <caatro270@gmail.com>
2022-02-07 09:24:31 -0300

    Limpeza de Código

--- app.py
+++ app.py
@@ -1,19 +1,16 @@
-from flask import Flask , request, jsonify
+from flask import Flask , jsonify
 from flask_cors import CORS, cross_origin   
 import mysql.connector
 app = Flask(__name__)
 CORS(app)
 @app.route('/')
 def fet():
+    #conexão com o DB
     con = mysql.connector.connect(host='kaligor.com.br',database='u201025653_graph',user='u201025653_root',password='eV*I6*L9/')
     cursor=con.cursor()
     if con.is_connected():
-        #html=''
-        #html+='<h1>Conectado ao servidor MySQL</h1> </br'
         cursor.execute("SELECT * FROM portifolio")
         selt=cursor.fetchall()
-
-        #html+='<p>'+selt[0][3]+'</p>'
         return jsonify(selt)
 
 
